bf5b97e1d82b56fdde2a763e49548430f89bc3bf
王超群 <bill_grim@outlook.com>
2019-07-20 20:50:01 +0800

    add [tags] to event when exceptions occur

--- src/main/java/org/logstashplugins/JavaFixProtocol.java
+++ src/main/java/org/logstashplugins/JavaFixProtocol.java
@@ -36,57 +36,61 @@ public class JavaFixProtocol implements Filter {
     @Override
     public Collection<Event> filter(Collection<Event> events, FilterMatchListener matchListener) {
         for (Event e : events) {
-            Object f = e.getField(sourceField);
-            if (f instanceof String) {
-                List<Integer> fieldList = Arrays.stream(dataDictionary.getOrderedFields()).boxed().collect(Collectors.toList());
-                String[] splits = ((String) f).split("\001");
-                List<Integer> unknownFields = new ArrayList<>();
-                for (String split : splits) {
-                    String[] kv = split.split("=");
-                    if (kv.length == 2) {
-                        int field = Integer.parseInt(kv[0]);
-                        Object value;
-                        FieldType fieldType = dataDictionary.getFieldType(field);
+            try {
+                Object f = e.getField(sourceField);
+                if (f instanceof String) {
+                    List<Integer> fieldList = Arrays.stream(dataDictionary.getOrderedFields()).boxed().collect(Collectors.toList());
+                    String[] splits = ((String) f).split("\001");
+                    List<Integer> unknownFields = new ArrayList<>();
+                    for (String split : splits) {
+                        String[] kv = split.split("=");
+                        if (kv.length == 2) {
+                            int field = Integer.parseInt(kv[0]);
+                            Object value;
+                            FieldType fieldType = dataDictionary.getFieldType(field);
 
-                        if (dataDictionary.isGroup(kv[1], field)) {
-                            // TODO fix group deal
+                            if (dataDictionary.isGroup(kv[1], field)) {
+                                // TODO fix group deal
 //                            DataDictionary.GroupInfo group = dataDictionary.getGroup(kv[1], field);
 //                            int[] orderedFields = group.getDataDictionary().getOrderedFields();
 
-                        } else if (dataDictionary.isField(field)) {
-                            switch (fieldType) {
-                                case INT:
-                                case DAYOFMONTH:
-                                    value = Integer.valueOf(kv[1]);
-                                    break;
-                                case PRICE:
-                                case FLOAT:
-                                case QTY:
-                                    value = Double.valueOf(kv[1]);
-                                    break;
-                                case BOOLEAN:
-                                    value = "Y".equals(kv[1]);
-                                    break;
-                                case UNKNOWN:
-                                    unknownFields.add(field);
-                                    value = kv[1];
-                                    break;
-                                default:
-                                    if (StringUtils.isBlank(dataDictionary.getValueName(field, kv[1]))) {
+                            } else if (dataDictionary.isField(field)) {
+                                switch (fieldType) {
+                                    case INT:
+                                    case DAYOFMONTH:
+                                        value = Integer.valueOf(kv[1]);
+                                        break;
+                                    case PRICE:
+                                    case FLOAT:
+                                    case QTY:
+                                        value = Double.valueOf(kv[1]);
+                                        break;
+                                    case BOOLEAN:
+                                        value = "Y".equals(kv[1]);
+                                        break;
+                                    case UNKNOWN:
+                                        unknownFields.add(field);
                                         value = kv[1];
-                                    } else {
-                                        value = dataDictionary.getValueName(field, kv[1]);
-                                    }
-                                    break;
+                                        break;
+                                    default:
+                                        if (StringUtils.isBlank(dataDictionary.getValueName(field, kv[1]))) {
+                                            value = kv[1];
+                                        } else {
+                                            value = dataDictionary.getValueName(field, kv[1]);
+                                        }
+                                        break;
+                                }
+                                e.setField(dataDictionary.getFieldName(field), value);
                             }
-                            e.setField(dataDictionary.getFieldName(field), value);
                         }
                     }
+                    if (unknownFields.size() > 0) {
+                        e.setField("unknownFields", unknownFields);
+                    }
+                    matchListener.filterMatched(e);
                 }
-                if (unknownFields.size() > 0) {
-                    e.setField("unknownFields", unknownFields);
-                }
-                matchListener.filterMatched(e);
+            } catch (Exception exception) {
+                e.setField("tags", Arrays.asList("_fix_parse_failure"));
             }
         }
         return events;
